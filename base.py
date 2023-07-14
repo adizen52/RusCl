3dd4b6dbf96f9b067db433875339c927a3aa84c1
adizen52 <adizen52@yandex.ru>
2023-07-14 16:05:11 +0300

    Доработка в классе users метод сохранения данных

--- base.py
+++ base.py
@@ -1,39 +1,50 @@
 import json
 
+
+class JSON_files():
+    base = 'base.json'
+    product = 'product.json'
+    users = 'users.json'
+    orders = 'orders.json'
+
+
 class Base():
+
+    def load_data(self, name_json):
+        with open(name_json) as file:
+            data = json.load(file)
+
+        return data
+
+    def dump_data(self, data, name_json):
+        with open(name_json, 'w') as file:
+            json.dump(data, file, indent=4)
+
     def update(self, *keys, **namekeys):
-        with open('base.json') as data:
-            file_json = json.load(data)
+
+        data = self.load_data(JSON_files.base)
 
         if len(keys) == 1:
-            file_json.setdefault(keys[0], {})
+            data.setdefault(keys[0], {})
         elif len(keys) == 2:
-            file_json.setdefault(keys[0], {}).setdefault(keys[1], {})
+            data.setdefault(keys[0], {}).setdefault(keys[1], {})
             if len(namekeys) == 1:
-                file_json[keys[0]][keys[1]] = namekeys['value']
+                data[keys[0]][keys[1]] = namekeys['value']
         elif len(keys) == 3:
-            file_json.setdefault(keys[0], {}).setdefault(keys[1], {}).setdefault(keys[2], {})
+            data.setdefault(keys[0], {}).setdefault(keys[1], {}).setdefault(keys[2], {})
             if len(namekeys) == 1:
-                file_json[keys[0]][keys[1]][keys[2]] = namekeys['value']
+                data[keys[0]][keys[1]][keys[2]] = namekeys['value']
 
-        with open('base.json', 'w') as data:
-            json.dump(file_json, data, indent= 4)
+        self.dump_data(data, JSON_files.base)
 
         return 'Данные успешно обновлены.'
 
+    def value_for_key(self, key):
+        data = self.load_data(JSON_files.base)
+        return data.get(key, 'Такого ключа нет.')
+
+
 item = Base()
 
-item.update('Dany', 'Addres', value='Nizniy Novgorod, Lenina, 3')
-item.update('Dany', 'Age', value=27)
-item.update('Dany', 'Car', value={'Marka': 'Porshe',
-                                  'Model': 'Cayen'})
-item.update('Dany', 'Car', 'Corobka', value='Robot')
-item.update('Dany', 'Age', value=24)
-item.update('Robby', 'Dany', value=15)
-item.update('Mariska', 'Age', value=32)
-item.update('Maroon', 'Age', value=18)
-item.update('Maroon', 'Address', value={'country' : 'Russia',
-                                        'city' : 'Moscow',
-                                        'street' : 'Lenina',
-                                        'house' : '2'})
-print(item.update('Maroon', 'Address', 'city', value='SPB'))
+print(item.value_for_key('Dany'))
+
